9562be9f37ce26b803506855a63869b2ede4e5f8
bobrocker20 <bond.davis@baruchmail.cuny.edu>
2014-09-27 23:00:48 -0400

    first commit

---
+++ Assets/Game/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// grid of nodes
+	public GameObject[,] nodeGrid;
+	public int nodeGridSizeX;
+	public int nodeGridSizeY;
+	public GameObject nodeObj;
+
+	// node min, max range
+	public int nodeMin;
+	public int nodeMax;
+
+	// node textures aka numbers
+	public NodeTexture[] nodeTextures;
+
+	// store dragging nodes
+	public ArrayList nodesDragged;
+
+	// singleton
+	public static GameManager gm_Instance = null;
+	public static GameManager Get() {
+		if (!gm_Instance) {
+			gm_Instance = (GameManager)FindObjectOfType(typeof(GameManager));
+		}
+		return gm_Instance;
+	}
+
+	// side-most position of grid
+	public float sideX;
+	public float sideY;
+
+	// Use this for initialization
+	void Start () {
+		// grid should be generated at origin
+		nodeGrid = new GameObject[nodeGridSizeX, nodeGridSizeY];
+		nodesDragged = new ArrayList();
+		sideX = (float)nodeGridSizeX / 2.0f;
+		sideY = (float)nodeGridSizeY / 2.0f;
+
+		// spawn nodes
+		for (int x=0; x<nodeGridSizeX; x++) {
+			for (int y=0; y<nodeGridSizeY; y++) {
+				nodeGrid[x,y] = spawnNode(x-sideX, y-sideY);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Spawns the node.
+	/// </summary>
+	private GameObject spawnNode(float x, float y) {
+		nodeObj.GetComponent<NodeManager>().posX = x;
+		nodeObj.GetComponent<NodeManager>().posY = y;
+		nodeObj.GetComponent<Transform>().position = new Vector3(x, y, 0.0f);
+		return Instantiate(nodeObj) as GameObject;
+	}
+}
---
+++ Assets/Game/NodeManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeManager : MonoBehaviour {
+	// position of node
+	public float posX;
+	public float posY;
+
+	// number of node
+	public int number;
+
+	// boolean to make sure OnMouseEnter is run once
+	private bool nodeEntered;
+
+	// Use this for initialization
+	void Start () {
+		randomizeNumber();
+		nodeEntered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Randomizes the number.
+	/// </summary>
+	/// <returns>The number.</returns>
+	private void randomizeNumber() {
+		number = Random.Range(GameManager.Get().nodeMin, GameManager.gm_Instance.nodeMax + 1);
+		// set texture to node
+		renderer.material.mainTexture = GameManager.Get().nodeTextures[0].numbers[number - 1];
+	}
+
+	// click node
+	void OnMouseDown() {
+		if (!nodeEntered) {
+			addNodeToNodesDragged();
+		}
+	}
+
+	// unclick node
+	void OnMouseUp() {
+		foreach (GameObject node in GameManager.Get().nodesDragged) {
+			node.renderer.material.shader = Shader.Find("Diffuse");
+			node.GetComponent<NodeManager>().nodeEntered = false;
+			node.GetComponent<NodeManager>().randomizeNumber();
+		}
+		GameManager.Get().nodesDragged.Clear();
+	}
+
+	// drag onto another node
+	void OnMouseEnter() {
+		if (!nodeEntered && GameManager.Get().nodesDragged.Count > 0) {
+			ArrayList nodesDragged = GameManager.Get().nodesDragged;
+			NodeManager nm = ((GameObject)nodesDragged.ToArray()[nodesDragged.Count - 1]).GetComponent<NodeManager>();
+			// limit to immediate adjacent node
+			if ((posX == nm.posX || posX == nm.posX - 1.0f || posX == nm.posX + 1.0f) && (posY == nm.posY || posY == nm.posY - 1.0f || posY == nm.posY + 1.0f)) {
+				// limit to number in next sequence
+				if (number == nm.number - 1 || number == nm.number + 1) {
+					addNodeToNodesDragged();
+				}
+			}
+		}
+	}
+
+	// exit hover over node
+	void OnMouseExit() {
+		nodeEntered = false;
+	}
+
+	// add node to nodes dragged array
+	private void addNodeToNodesDragged() {
+		nodeEntered = true;
+		GameManager.Get().nodesDragged.Add(gameObject);
+		renderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+	}
+}
